bec4b01a095a2b20f04495cb2866122247cbbb2d
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-21 13:12:34 +0300

    add button x

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -3,23 +3,29 @@ import React from 'react';
 export const Todolist = () => {
     return (
 
+        <div>
+            <h3>What to learn</h3>
             <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><button>X</button><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><button>X</button><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><button>X</button><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
+                <input/>
+                <button>+</button>
             </div>
+            <ul>
+                <li>
+                    <button>X</button>
+                    <input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                <li>
+                    <button>X</button>
+                    <input type="checkbox" checked={true}/> <span>JS</span></li>
+                <li>
+                    <button>X</button>
+                    <input type="checkbox" checked={false}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
 
     );
 };
