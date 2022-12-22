1d12e3d8c84393a835dba1b7c538d6c0433e31cd
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 12:59:42 +0300

    look better

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -36,9 +36,15 @@ export function Todolist(props: PropsType) {
         props.addTask(newTaskTitle)
         setNewTaskTitle("")
     }
-    const onAllClickHandler = () => {props.changeFilter("all")}
-    const onActiveClickHandler = () => {props.changeFilter("active")}
-    const onCompletedClickHandler = () => {props.changeFilter("completed")}
+    const onAllClickHandler = () => {
+        props.changeFilter("all")
+    }
+    const onActiveClickHandler = () => {
+        props.changeFilter("active")
+    }
+    const onCompletedClickHandler = () => {
+        props.changeFilter("completed")
+    }
 
     return <div>
         <h3>{props.title}</h3>
@@ -52,7 +58,9 @@ export function Todolist(props: PropsType) {
             {
                 props.tasks.map(t => {
 
-                    const onRemoveHandler= () =>{props.removeTask(t.id)}
+                    const onRemoveHandler = () => {
+                        props.removeTask(t.id)
+                    }
 
                     return <li key={t.id}>
                         <input type="checkbox" checked={t.isDone}/>
