127f1e5e5076cc57a4b41ec6f09cfcb366626b90
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 12:46:55 +0300

    newTaskHandler

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -12,24 +12,31 @@ type PropsType = {
     tasks: Array<TaskType>
     removeTask: (taskId: string) => void
     changeFilter: (value: FilterValuesType) => void
-    addTask: (title:string) => void
+    addTask: (title: string) => void
 }
 
 export function Todolist(props: PropsType) {
 
     const [newTaskTitle, setNewTaskTitle] = useState("")
 
-    const onChangeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
-        {setNewTaskTitle(e.currentTarget.value)}
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        {
+            setNewTaskTitle(e.currentTarget.value)
+        }
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        {
+            if (e.key === "Enter") {
+                props.addTask(newTaskTitle)
+                setNewTaskTitle("")
+            }
+        }
+    }
+    const newTaskHandler = () => {
+        props.addTask(newTaskTitle)
+        setNewTaskTitle("")
     }
 
-    const onKeyPressHandler = (e:KeyboardEvent<HTMLInputElement>) => {
-        {if (e.key === "Enter")
-        {props.addTask(newTaskTitle)
-            setNewTaskTitle("")
-        }
-        }
-    }
 
     return <div>
         <h3>{props.title}</h3>
@@ -38,10 +45,7 @@ export function Todolist(props: PropsType) {
                    onChange={onChangeHandler}
                    onKeyDown={onKeyPressHandler}
             />
-            <button onClick={() => {
-                props.addTask(newTaskTitle)
-                setNewTaskTitle("")
-            }}>+
+            <button onClick={newTaskHandler}>+
             </button>
         </div>
         <ul>
