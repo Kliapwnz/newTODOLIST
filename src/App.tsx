59039243747a231c8e26130863d42637462a8059
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 12:01:15 +0300

    string title

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     }
 
     function addTask(title:string) {
-        let newTask = {id: v1(), title: "New Task", isDone: false}
+        let newTask = {id: v1(), title: title, isDone: false}
         let newTasks = [newTask, ...tasks]
         setTasks(newTasks)
     }
