8c0ffb7ab5ef8970f8a4321df2b3973b74bee00c
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-03 18:44:57 +0300

    e.title

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,19 @@ import {Todolist} from './Todolist';
 import {v1} from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
+type TodolistsType={id:string,title:string,filter:FilterValuesType}
+
 
 function App() {
+
+
+
+    let[todolists,setTodolists]=useState<Array<TodolistsType>>([
+        {id:v1(),title:'What to learn',filter:'all'},
+        {id:v1(),title:'What to buy',filter:'all'},
+    ])
+
+
 
     let [tasks, setTasks] = useState([
         {id: v1(), title: "HTML&CSS", isDone: true},
@@ -54,14 +65,21 @@ function App() {
 
     return (
         <div className="App">
-            <Todolist title="What to learn"
-                      tasks={tasksForTodolist}
-                      removeTask={removeTask}
-                      changeFilter={changeFilter}
-                      addTask={addTask}
-                      changeTaskStatus={changeStatus}
-                      filter={filter}
-            />
+            {todolists.map((e)=>{
+                return(
+                <Todolist title={e.title}
+                          tasks={tasksForTodolist}
+                          removeTask={removeTask}
+                          changeFilter={changeFilter}
+                          addTask={addTask}
+                          changeTaskStatus={changeStatus}
+                          filter={filter}
+                />
+            )
+
+            })}
+
+
         </div>
     );
 }
