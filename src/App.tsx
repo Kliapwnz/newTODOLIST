38b1abfab5af1f0f487a4e8c2940e9f0df4d9005
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-15 12:52:56 +0300

    todolistID

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,6 @@ type TodolistType = {
 
 function App() {
 
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ]);
-
-
     function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id != id);
         setTasks(filteredTasks);
@@ -42,18 +33,36 @@ function App() {
     }
 
     function changeFilter(value: FilterValuesType, todolistId: string) {
-let todolist = todoLists.find(e=> e.id === todolistId)
-        if(todolist) {
+        let todolist = todoLists.find(e => e.id === todolistId)
+        if (todolist) {
             todolist.filter = value
             setTodoLists([...todoLists])
         }
     }
 
+    let todoListId1 = v1()
+    let todoListId2 = v1()
+
     let [todoLists, setTodoLists] = useState<Array<TodolistType>>([
         {id: v1(), title: "What to learn", filter: "all"},
         {id: v1(), title: "What to Buy", filter: "all"}
     ])
-
+    let [tasks, setTasks] = useState({
+        [todoListId1]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todoListId2]: [
+            {id: v1(), title: "Milk", isDone: true},
+            {id: v1(), title: "Cola", isDone: true},
+            {id: v1(), title: "Meat", isDone: false},
+            {id: v1(), title: "Beer", isDone: false},
+            {id: v1(), title: "Burger", isDone: false},
+        ]
+    })
 
     return (
         <div className="App">
@@ -62,10 +71,10 @@ let todolist = todoLists.find(e=> e.id === todolistId)
                     let tasksForTodolist = tasks;
 
                     if (e.filter === "active") {
-                        tasksForTodolist = tasks.filter(t => t.isDone === false);
+                        tasksForTodolist = tasks[e.id].filter(t => t.isDone === false);
                     }
                     if (e.filter === "completed") {
-                        tasksForTodolist = tasks.filter(t => t.isDone === true);
+                        tasksForTodolist = tasks[e.id].filter(t => t.isDone === true);
                     }
                     return (
                         <Todolist
