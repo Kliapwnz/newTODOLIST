e4852cc8ee77ee56e6148bad481db738013d8681
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-17 19:23:59 +0300

    ButtonAppBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
 import {AddItemForm} from './AddItemForm';
+import {ButtonAppBar} from "./ButtonAppBar";
 
 export type FilterValuesType = "all" | "active" | "completed";
 type TodolistType = {
@@ -123,6 +124,7 @@ function App() {
 
     return (
         <div className="App">
+            <ButtonAppBar/>
             <AddItemForm addItem={addTodolist}/>
             {
                 todolists.map(tl => {
