557469cdf62edd509dc80a7e02bc878e3e41ce8c
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-24 18:46:13 +0300

    lesson 8

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,13 +1,16 @@
 import {TodolistType} from "../App";
 
-export const TodolistsReducer=(state:TodolistType[], action:any)=>{
+export const TodolistsReducer=(state:TodolistType[], action:TsarType)=>{
     switch (action.type){
-        case "XXX": {
-            return state
-        }
+        case "REMOVE-TODOLIST":{
+            return state.filter(e=>e.id!==action.payload.id)
+    }
         default: return state
     }
 }
+type TsarType = removeTodolistACType
+
+
 type removeTodolistACType= ReturnType<typeof removeTodolistAC>
 export const removeTodolistAC=(id:string)=>{
     return{
