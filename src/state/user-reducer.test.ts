64d9ee1aeb137ba2767ffa5e757a7557f147b910
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-26 16:39:24 +0300

    user-reducer.test.ts

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -1,14 +1,14 @@
 import {userReducer} from "./user-reducer";
 
 test("user reducer should increment only age", ()=>{
-    const startState ={age:20, childrenCount: 26, name: "Evgenych"};
+    const startState ={age:20, childrenCount: 2, name: "KLIApwnz"};
 
     const endState = userReducer(startState,{type:"INCREMENT-AGE"})
 
     expect(endState.age).toBe(21)
-    expect(endState.childrenCount).toBe(26)
+    expect(endState.childrenCount).toBe(2)
 })
 
 test("user reducer should increment only childrenCount", ()=>{
-    const startState ={age:20, childrenCount: 2, name:"Evgenych"};
+    const startState ={age:20, childrenCount: 2, name:"KLIApwnz"};
 })
