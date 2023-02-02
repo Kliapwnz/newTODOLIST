14aa27b04d85cc3773d0c190ec7cd8f3cf7e8461
Evgeniy Klimovch <kliasanik@gmail.com>
2023-02-02 20:42:03 +0300

    user-reducer.ts

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -1,19 +1,23 @@
 type StateType = {
-    age:number
-    childrenCount:number
-    name:string
+    age: number
+    childrenCount: number
+    name: string
 }
 type ActionType = {
-    type:string
-    [key:string]:any
+    type: string
+    [key: string]: any
 }
 
 
-export const userReducer = (state:StateType, action:ActionType) => {
-   switch (action.type){
-       case "BlaBla":
-       case "KLIA":
-       default:
-           throw new Error("Stop this")
-   }
+export const userReducer = (state: StateType, action: ActionType) => {
+    switch (action.type) {
+        case "INCREMENT-AGE":
+            state.age = state.age + 1;
+            return state;
+        case "INCREMENT-CHILDREN-COUNT":
+            state.childrenCount = state.childrenCount + 1;
+            return state;
+        default:
+            throw new Error("Stop this")
+    }
 }
