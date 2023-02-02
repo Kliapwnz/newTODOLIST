bd99c63a2f72bbc450036a95bfdbd92169f12087
Evgeniy Klimovch <kliasanik@gmail.com>
2023-02-02 21:19:09 +0300

    user-reducer.test.ts

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -17,3 +17,12 @@ test("user reducer should increment only childrenCount", ()=>{
     expect(endState.childrenCount).toBe(27)
     expect(endState.age).toBe(22)
 })
+
+test("user reducer should increment only childrenCount", ()=>{
+    const startState = {age: 22, childrenCount: 26, name: "Bradley"}
+    const newName = "Evgeniy"
+    const endState = userReducer(startState, {type: "CHANGE-NAME", newName})
+
+    expect(endState.name).toBe(newName)
+})
+
