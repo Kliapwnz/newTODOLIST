fd6925636194a6081198e91d830f9fa79e277137
Evgeniy Klimovch <kliasanik@gmail.com>
2023-02-02 20:29:18 +0300

    lesson 8

--- src/App.tsx
+++ src/App.tsx
@@ -32,11 +32,13 @@ function App() {
     let [tasks, setTasks] = useState<TasksStateType>({
         [todolistId1]: [
             {id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true}
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "React", isDone: false}
         ],
         [todolistId2]: [
             {id: v1(), title: "Milk", isDone: true},
-            {id: v1(), title: "React Book", isDone: true}
+            {id: v1(), title: "React Book", isDone: false},
+            {id: v1(), title: "Cola", isDone: true}
         ]
     });
 
