6d2b994e3f4f6a8c82cf475637b2c9477a1fff5c
Evgeniy Klimovch <kliasanik@gmail.com>
2023-02-02 20:36:17 +0300

    user-reducer.ts

---
+++ src/state/user-reducer.ts
@@ -0,0 +1,8 @@
+export const userReducer = (state:any, action:any) => {
+   switch (action.type){
+       case "BlaBla":
+       case "KLIA":
+       default:
+           throw new Error("Stop this")
+   }
+}
